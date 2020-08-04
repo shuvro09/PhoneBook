17e5a615b5b0c9a4ba72216b4c15b89290746369
Shuvrajyoti Paul <shuvro997@gmail.com>
2020-08-05 04:07:17 +0530

    added delete button

--- PhoneBook-client/src/components/ContactList.js
+++ PhoneBook-client/src/components/ContactList.js
@@ -1,21 +1,57 @@
 import React from 'react';
-function contactList({ data }) {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+//data=fetched data, search=filtered data, update=set Fetched data , updateSearch=setSearch
+const handleDelete = (id, data, update, search, updateSearch) => {
+    const options = {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ id: id })
+    }
+
+    //http delete request
+    fetch("http://192.168.1.7:3001", options)
+        .then(response => response.json())
+        .then(res => {
+            if (res.deletedCount) {
+                update(data.filter((elem) => elem._id !== id));
+
+                //checks if the delete button was clicke from inside contactList or filteredContactList
+                if (updateSearch !== undefined)
+                    updateSearch(search.filter((elem) => elem._id !== id))
+            }
+            else {
+                console.log("could not find data")
+            }
+        })
+        .catch(error => { console.log(error); })
+}
+
+//component for showing the entire data 
+// data=fetched data , update=set fetch data
+function contactList({ data, update }) {
     console.log(data + "form data")
     if (data === undefined)
         return (<p>Loading</p>)
     if (data.length !== 0) {
-        var contactList = data.map((info) => {
+        const contactList = data.map((info) => {
             return (
 
-                <li key={info._id}>
+                <li className="contact" key={info._id}>
                     <h3>{info.name}</h3>
                     <small>{info.number}</small>
+                    <FontAwesomeIcon className="deleteIcon"
+                        onClick={(e) => {
+                            handleDelete(info._id, data, update)
+                        }} icon={faTrashAlt} />
                 </li>
 
             )
         })
         return (
-            <ul>
+            <ul className="contactList">
                 {contactList}
             </ul>
         )
@@ -24,18 +60,21 @@ function contactList({ data }) {
 }
 function FilteredContacts(props) {
     if (props.data.length !== 0) {
-        var contactList = props.data.map((info) => {
+        const contactList = props.data.map((info) => {
             return (
 
-                <li key={info._id}>
+                <li className="contact" key={info._id}>
                     <h3>{info.name}</h3>
-                    <small>{info.number}</small>
+                    <small>{info.number} </small>
+                    <FontAwesomeIcon className="deleteIcon" onClick={(e) => {
+                        handleDelete(info._id, props.fdata, props.update, props.data, props.updateSearch)
+                    }} icon={faTrashAlt} />
                 </li>
 
             )
         })
         return (
-            <ul>
+            <ul className="contactList">
                 {contactList}
             </ul>
         )
